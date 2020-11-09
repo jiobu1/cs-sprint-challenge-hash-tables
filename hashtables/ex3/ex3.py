b0034294336a860bb8f1ec85a6be52380a6b5ed1
Jisha Obukwelu <jisha@obukwelu.com>
2020-11-08 22:05:47 -0500

    Done

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,20 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+
+    for array in arrays:
+        for i in array:
+            if i in cache:
+                cache[i] += 1
+            else:
+                cache[i] = 1
+    
+    result = []
+
+    for k, v in cache.items():
+        if v == len(arrays):
+            result.append(k)
 
     return result
 
@@ -15,3 +29,11 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
+"""
+..
+----------------------------------------------------------------------
+Ran 2 tests in 3.049s
+
+OK
+"""
